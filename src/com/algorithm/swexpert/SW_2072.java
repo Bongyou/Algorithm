cca22ed0e12e7f87c04dcdeb5658e939b01b10b6
봉유정 <qhddbwjd@naver.com>
2021-07-15 15:51:20 +0900

    solve swexpert 2072

--- src/com/algorithm/swexpert/SW_2072.java
+++ src/com/algorithm/swexpert/SW_2072.java
@@ -1,7 +1,33 @@
 package com.algorithm.swexpert;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class SW_2072 {
-    public static void main(String[] args) {
-        System.out.println("hello world");
+    static int T;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        T = Integer.parseInt(br.readLine().trim());
+
+        for (int t = 1; t <= T; t++) {
+            st = new StringTokenizer(br.readLine().trim());
+
+            int sum = 0;
+            int n = 0;
+
+            for (int i = 0; i < 10; i++) {
+                n = Integer.parseInt(st.nextToken().trim());
+
+                if (n % 2 == 1) {
+                    sum += n;
+                }
+            }
+
+            System.out.println("#" + t + " " + sum);
+        }
     }
 }
